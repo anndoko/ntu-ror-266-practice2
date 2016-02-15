19680d33151989e1f775bd4abad2b76d33eb7a02
Anndo <handcuffanndo@gmail.com>
2016-02-16 00:13:04 +0800

    Create validations

--- youflix/app/models/user.rb
+++ youflix/app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   has_many :videos
+
+  # Validate names
+  validates :name, presence: true 
+  validates :name, length: { minimum: 2 }
+  validates :name, length: { maximum: 200 }
+
+  # Validate emails
+  validates :email, presence: true
 end
