c5b8a763d55fb3fe1da5d4165f1225e231aa02c9
Anndo <handcuffanndo@gmail.com>
2016-02-16 23:30:05 +0800

    Fix: tab space

--- youflix/db/migrate/20160215145626_add_users_table.rb
+++ youflix/db/migrate/20160215145626_add_users_table.rb
@@ -1,10 +1,10 @@
 class AddUsersTable < ActiveRecord::Migration
   def change
-  	create_table :users do |t|
+    create_table :users do |t|
       t.string :name
       t.string :email
-  	end
+    end
 
-  	add_column :videos, :user_id, :integer
+    add_column :videos, :user_id, :integer
   end
 end
